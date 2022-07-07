69594b260a83c201bed0810b57e7781d67bdad8f
unknown <rocketpowerswillian@gmail.com>
2022-07-07 20:32:57 -0300

    simple full-stack project

---
+++ src/main/java/todoPackages/domain/Todo.java
@@ -0,0 +1,14 @@
+package todoPackages.domain;
+
+import java.time.LocalDate;
+
+public class Todo {
+	
+	private Integer id;
+	private String title;
+	private String description;
+	private LocalDate dataParaFinalizar;
+	private Boolean finalizado=false;
+	
+
+}
